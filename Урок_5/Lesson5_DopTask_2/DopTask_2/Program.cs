3cb622af2dcd2fb28b452e1f5500efc66dc3aff6
natalyashuk <nata.lyashuk6@gmail.com>
2020-10-07 14:24:19 +0300

    add dop task2

---
+++ Урок_5/Lesson5_DopTask_2/DopTask_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DopTask_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool A = true; 
+            bool B = false;
+            bool result1 = A || B;
+            bool result2 = !(!A&&!B);
+           
+            Console.WriteLine("Значение A{0}|| B{1}={2}", A, B, result1);
+            Console.WriteLine("Значение A{0}|| B{1}={2}", A, B, result2);
+
+            Console.ReadKey();
+        }
+    }
+}
