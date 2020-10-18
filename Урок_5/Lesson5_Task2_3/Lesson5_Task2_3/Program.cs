e137960a8e2629218840d048fd2df29ca0c9f598
natalyashuk <nata.lyashuk6@gmail.com>
2020-10-18 20:14:23 +0300

    вариант 3 в lesson5_task2

--- Урок_5/Lesson5_Task2_3/Lesson5_Task2_3/Program.cs
+++ Урок_5/Lesson5_Task2_3/Lesson5_Task2_3/Program.cs
@@ -6,7 +6,19 @@ namespace Lesson5_Task2_3
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Введите число");
+            byte a = Convert.ToByte(Console.ReadLine());
+            byte result = (byte)(a << 7);
+
+            if (result==0)
+            {
+                Console.WriteLine("Вы ввели четное число");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели нечетное число");
+            }
+            Console.ReadKey();
         }
     }
 }
